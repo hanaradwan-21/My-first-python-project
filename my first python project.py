1c9b4d07d0c496a965cea2d260685dc211d7540c
Hana <hannaradwan6@gmail.com>
2025-09-01 11:57:44 +0300

    Add files via upload

---
+++ my first python project.py
@@ -0,0 +1,6 @@
+print('---Age in Days Calculator---')
+age_years=int(input("Enter your age in years : "))
+age_days=age_years*365
+age_months=age_years*12
+age_hours=age_days*24
+print(f'you have lived {age_days} days,{age_months} months and {age_hours} hours')
